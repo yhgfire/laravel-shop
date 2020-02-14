84f1b449c3025ac631659b4d90c93bffc41a3532
vagrant <vagrant@homestead>
2020-02-14 09:11:23 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Auth::routes();
 
 Auth::routes(['verify' => true]);
 
+Route::group(['middleware'=>['auth','verified']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
