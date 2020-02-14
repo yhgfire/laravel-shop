72b7681cf1dfa0f6245413b368c9fbabee7c33bf
vagrant <vagrant@homestead>
2020-02-14 08:09:24 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
+//Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
+Auth::routes(['verify' => true]);
 
