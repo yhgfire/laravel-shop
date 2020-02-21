5e1cfd08d7dd95fb27157cdaa16c0ed5fcd1f8c4
vagrant <vagrant@homestead>
2020-02-21 10:07:55 +0000

    NEW3

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 //Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
